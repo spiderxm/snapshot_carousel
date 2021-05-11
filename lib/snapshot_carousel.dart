1fced5bdf446e4c634851f9cd8c2fcd2c8fa0129
spiderxm <mrigank.rkee@rediffmail.com>
2021-05-11 17:39:18 +0530

    Adding more features to Snapshot Carousel

--- lib/snapshot_carousel.dart
+++ lib/snapshot_carousel.dart
@@ -8,23 +8,35 @@ class SnapShotCarousel {
   /// Returns a image Carousel
   static Widget snapShotCarousel(List<Image> imageList,
       {Color backgroundColor = Colors.white,
-        Color selectedImageBorderColor = Colors.orange,
-        Color unselectedImageBorderColor = Colors.grey,
-        double imageHeight = 250,
-        double selectedImageBorderWidth = 2,
-        double unselectedImageBorderWidth = .5,
-        double borderRadius = 20,
-        double featureImageBorderRadius = 0,
-        Border featureImageBorder,
-        List<BoxShadow> featureImageBoxShadow}) {
+      Color selectedImageBorderColor = Colors.orange,
+      Color unselectedImageBorderColor = Colors.grey,
+      double featureImageHeight = 250,
+      double selectedImageBorderWidth = 2,
+      double unselectedImageBorderWidth = .5,
+      double borderRadius = 20,
+      double featureImageBorderRadius = 0,
+      double placeholderImageHeight = 84,
+      Border featureImageBorder,
+      List<BoxShadow> featureImageBoxShadow}) {
+    /// Return a Container with text if no Image is passed.
+    if (imageList.length == 0) {
+      return Container(
+        child: Text("Please pass an non empty List of images."),
+      );
+    }
     return ImageCarouselWidget(
       images: imageList,
       selectedImageBorderColor: selectedImageBorderColor,
       unselectedImageBorderColor: unselectedImageBorderColor,
-      imageHeight: imageHeight,
+      featureImageHeight: featureImageHeight,
       selectedImageBorderWidth: selectedImageBorderWidth,
       unselectedImageBorderWidth: unselectedImageBorderWidth,
       borderRadius: borderRadius,
+      featureImageBorderRadius: featureImageBorderRadius,
+      featureImageBoxShadow: featureImageBoxShadow,
+      featureImageBorder: featureImageBorder,
+      backgroundColor: backgroundColor,
+      placeholderImageHeight: placeholderImageHeight,
     );
   }
 }
